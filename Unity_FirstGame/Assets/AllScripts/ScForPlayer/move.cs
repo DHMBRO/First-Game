a9ed989fb4ae0df4d37a9930f44f1c492ab03ab3
ArtemLabutkin <127979955+ArtemLabutkin@users.noreply.github.com>
2023-03-24 22:09:22 +0200

    add move

--- Unity_FirstGame/Assets/AllScripts/ScForPlayer/move.cs
+++ Unity_FirstGame/Assets/AllScripts/ScForPlayer/move.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class move : MonoBehaviour
 {
+    [SerializeField] float Force;
+    [SerializeField] ForceMode ForceMode;
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        rb = GetComponent<Rigidbody>();
+    } 
 
     // Update is called once per frame
     void Update()
     {
-        
+        float vert = Input.GetAxis("Vertical");
+        float hore = Input.GetAxis("Horizontal");
+        Vector3 Forse = new Vector3(vert * Force, 0, hore * Force);
+        rb.AddRelativeForce(Forse, ForceMode);
+
     }
 }
