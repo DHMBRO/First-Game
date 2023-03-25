05682a1a0a91a0b4526c0a7a209330b3d418898c
ArtemLabutkin <127979955+ArtemLabutkin@users.noreply.github.com>
2023-03-25 18:03:23 +0200

    sdrftgyhuj

--- Unity_FirstGame/Assets/AllScripts/ScForPlayer/Camera.cs
+++ Unity_FirstGame/Assets/AllScripts/ScForPlayer/Camera.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveCamera : MonoBehaviour
+public class Camera : MonoBehaviour
 {
     [SerializeField] GameObject TargetCamera;
     [SerializeField] Vector3 Offset;
@@ -23,6 +23,7 @@ public class MoveCamera : MonoBehaviour
         transform.localEulerAngles.x - Input.GetAxis("Mouse Y") * MouseSens, 
         transform.localEulerAngles.y + Input.GetAxis("Mouse X") * MouseSens, 
         0.0f);
+        TargetCamera.transform.localEulerAngles = new Vector3(0.0f, TargetCamera.transform.localEulerAngles.y + Input.GetAxis("Mouse X") * MouseSens, 0.0f);
 
         Vector3 TargetPosition = TargetCamera.transform.TransformPoint(Offset);
         transform.position = TargetPosition - transform.forward * MoveBack;
