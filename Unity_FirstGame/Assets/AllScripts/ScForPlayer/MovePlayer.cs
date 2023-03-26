f2e21f71eb87c338575c89ca0b6c72ace30a47ec
ArtemLabutkin <127979955+ArtemLabutkin@users.noreply.github.com>
2023-03-26 14:22:41 +0300

    jhngbfvdc

--- Unity_FirstGame/Assets/AllScripts/ScForPlayer/MovePlayer.cs
+++ Unity_FirstGame/Assets/AllScripts/ScForPlayer/MovePlayer.cs
@@ -4,96 +4,146 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
-
     [SerializeField] float Force = 1.0f;
+    [SerializeField] float JumpForce = 1.0f;
     [SerializeField] ForceMode MyForceMode;
     [SerializeField] Rigidbody MyRigidbody;
-
     [SerializeField] protected Transform CameraTransform;
     [SerializeField] protected float Sens = 1.0f;
     bool dontInJamp = true;
     int jumpcount;
-
+    bool chengebutton = false;
     void Start()
     {
         MyRigidbody = GetComponent<Rigidbody>();
     }
-
-   
-
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Plane"))
         {
-            
             if (!dontInJamp)
             {
                 dontInJamp = true;
                 Debug.Log("1" + dontInJamp);
             }
-            
-
         }
     }
-   
-
-        void Update()
+    void Update()
+    {
+        float MoveVertical = Input.GetAxis("Vertical");
+        float MoveHorizontal = Input.GetAxis("Horizontal");
+        if (Input.GetKeyUp(KeyCode.W))
         {
-            float MoveVertical = Input.GetAxis("Vertical");
-            float MoveHorizontal = Input.GetAxis("Horizontal");
-
-            if (Input.GetKey(KeyCode.W))
+            if (dontInJamp)
             {
-                MyRigidbody.drag = 0f;
-                Vector3 ForceFronte = new Vector3(0.0f, 0.0f, MoveVertical * Force);
-                MyRigidbody.AddRelativeForce(ForceFronte, MyForceMode);
-
-            }
-            else if (Input.GetKey(KeyCode.S))
-            {
-                MyRigidbody.drag = 0f;
-                Vector3 ForceBack = new Vector3(0.0f, 0.0f, MoveVertical * Force);
-                MyRigidbody.AddRelativeForce(ForceBack, MyForceMode);
-
-            }
-
-            else if (Input.GetKey(KeyCode.A))
-            {
-                MyRigidbody.drag = 0f;
-
-                Vector3 ForceRight = new Vector3(MoveHorizontal * Force, 0.0f, 0.0f);
-                MyRigidbody.AddRelativeForce(ForceRight, MyForceMode);
-
-            }
-            else if (Input.GetKey(KeyCode.D))
-            {
-                MyRigidbody.drag = 0f;
-                Vector3 ForceLeft = new Vector3(MoveHorizontal * Force, 0.0f, 0.0f);
-                MyRigidbody.AddRelativeForce(ForceLeft, MyForceMode);
-
+                MyRigidbody.drag = 500000.0f;
             }
             else
             {
-                if (dontInJamp)
-                {
-                    MyRigidbody.drag = 500.0f;
-                }
-                else
-                {
-                    MyRigidbody.drag = 0f;
-                }
+                MyRigidbody.drag = 0f;
             }
-            if (Input.GetKey(KeyCode.Space) && dontInJamp)
+            chengebutton = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            if (dontInJamp)
+            {
+                MyRigidbody.drag = 500000.0f;
+            }
+            else
             {
                 MyRigidbody.drag = 0f;
-                jumpcount = 100;
-                dontInJamp = false;
             }
-            if (jumpcount > 0.0f)
+            chengebutton = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.A))
+        {
+            if (dontInJamp)
             {
-                Vector3 addForce = new Vector3(0.00f, 2f * Force, 0f);
-                MyRigidbody.AddRelativeForce(addForce, MyForceMode);
-                jumpcount--;
-            }           
-        }            
+                MyRigidbody.drag = 500000.0f;
+            }
+            else
+            {
+                MyRigidbody.drag = 1f;
+            }
+            chengebutton = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
+        {
+            if (dontInJamp)
+            {
+                MyRigidbody.drag = 500000.0f;
+            }
+            else
+            {
+                MyRigidbody.drag = 1f;
+            }
+            chengebutton = true;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (!chengebutton)
+            {
+                MyRigidbody.drag = 1f;
+                Vector3 ForceFronte = new Vector3(0.0f, 0.0f, MoveVertical * Force);
+                MyRigidbody.AddRelativeForce(ForceFronte, MyForceMode);
+            }
+            chengebutton = false;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            if (!chengebutton)
+            {
+                MyRigidbody.drag = 1f;
+                Vector3 ForceBack = new Vector3(0.0f, 0.0f, MoveVertical * Force);
+                MyRigidbody.AddRelativeForce(ForceBack, MyForceMode);
+            }
+            chengebutton = false;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            if (!chengebutton)
+            {
+                MyRigidbody.drag = 1f;
+
+                Vector3 ForceRight = new Vector3(MoveHorizontal * Force, 0.0f, 0.0f);
+                MyRigidbody.AddRelativeForce(ForceRight, MyForceMode);
+            }
+            chengebutton = false;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            if (!chengebutton)
+            {
+                MyRigidbody.drag = 1f;
+                Vector3 ForceLeft = new Vector3(MoveHorizontal * Force, 0.0f, 0.0f);
+                MyRigidbody.AddRelativeForce(ForceLeft, MyForceMode);
+            }
+            chengebutton = false;
+        }
+        else
+        {
+            if (dontInJamp)
+            {
+                MyRigidbody.drag = 500.0f;
+            }
+            else
+            {
+                MyRigidbody.drag = 1f;
+            }
+            chengebutton = false;
+        }
+        if (Input.GetKey(KeyCode.Space) && dontInJamp)
+        {
+            MyRigidbody.drag = 1f;
+            jumpcount = 100;
+            dontInJamp = false;
+        }
+        if (jumpcount > 0.0f)
+        {
+            Vector3 addForce = new Vector3(0.00f, 2f * JumpForce, 0f);
+            MyRigidbody.AddRelativeForce(addForce, MyForceMode);
+            jumpcount--;
+        }
+    }        
 }
