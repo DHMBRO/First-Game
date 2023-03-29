38e6d329e5b99bd9423bbb6f1d30ed89ed3b3a99
awaqerer <127976085+awaqerer@users.noreply.github.com>
2023-03-29 15:13:42 +0300

    wow 1 more script

--- Unity_FirstGame/Assets/AllScripts/ScForM4/ShopForM4.cs
+++ Unity_FirstGame/Assets/AllScripts/ScForM4/ShopForM4.cs
@@ -1,18 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ShopForM4 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] public int M4ShopAmmo = 30;
+  
 }
