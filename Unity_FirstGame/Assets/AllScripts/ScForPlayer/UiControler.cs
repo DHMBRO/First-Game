8f35742571e7a82d55506be1f068def25c9e54a2
ArtemLabutkin <127979955+ArtemLabutkin@users.noreply.github.com>
2023-04-13 17:02:43 +0300

    egfdsc

--- Unity_FirstGame/Assets/AllScripts/ScForPlayer/UiControler.cs
+++ Unity_FirstGame/Assets/AllScripts/ScForPlayer/UiControler.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UiControler : MonoBehaviour
 {
-    
+    [SerializeField] Image scopeUi;
+    [SerializeField] Image inventorySlot1x1;
+    [SerializeField] Image inventorySlot2x1;
     void Start()
     {
-        
+        scopeUi.enabled = true;
+        inventorySlot1x1.enabled = false;
+        inventorySlot2x1.enabled = false;
     }
 
     
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            scopeUi.enabled = !scopeUi.enabled;
+            inventorySlot1x1.enabled = !scopeUi.enabled;
+            inventorySlot2x1.enabled = !scopeUi.enabled;
+            Cursor.lockState = CursorLockMode.None;
+        }
     }
 }
