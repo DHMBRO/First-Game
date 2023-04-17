1729dc46bbbc07b84f752cd45437af78155413cd
DM <127616087+DHMBRO@users.noreply.github.com>
2023-04-17 15:03:14 +0200

    Add rotate player hand

--- Unity_FirstGame/Assets/AllScripts/ScForPlayer/CamFirstFace.cs
+++ Unity_FirstGame/Assets/AllScripts/ScForPlayer/CamFirstFace.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class CamFirstFace : MonoBehaviour
 {    
     [SerializeField] private Transform Player;
+    [SerializeField] private Transform PlayerHand;
     [SerializeField] public Transform ObjectRay;
     [SerializeField] private float Sens = 0.5f;
     [SerializeField] UiControler uiControler;
@@ -29,6 +30,19 @@ public class CamFirstFace : MonoBehaviour
                 Player.Rotate(MouseX * new Vector3(0.0f, Sens, 0.0f));
             }
         }
+        if(PlayerHand)
+        {
+            RaycastHit HitResult;
+            Vector3 TargertPoint = gameObject.position * gameObject.forward * 100.0f; 
+            if(Physics.Raycast(gameObject.position, gameObject.forward * 100.0f))
+            {
+                TargertPoint = HitResult.point;
+            }
+            Quaternion RotateToTarget = Quaternion.LookRotation(TargertPoint - PlayerHand.position);
+            PlayerHand.rotation = Quaternion.Lerp(PlayerHand.rotation, RotateToTarget, 0.1f);
+        }
+
+
     }
     
    
