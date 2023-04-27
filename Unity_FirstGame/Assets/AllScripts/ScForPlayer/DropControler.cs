4f636fc28a8ef5398728ff5cc816718cf4e49919
DM <127616087+DHMBRO@users.noreply.github.com>
2023-04-27 08:42:59 +0200

    Update

--- Unity_FirstGame/Assets/AllScripts/ScForPlayer/DropControler.cs
+++ Unity_FirstGame/Assets/AllScripts/ScForPlayer/DropControler.cs
@@ -2,17 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DropControler : MonoBehaviour
+public class DropControler : MethodsFromDevelopers
 {
-    // Start is called before the first frame update
+    [SerializeField] Transform PointForDrop;
+    [SerializeField] GameObject ObjerctToDrop;
+    
+    [SerializeField] SlotControler ControlerForSklots;  
+
     void Start()
     {
+        if(ControlerForSklots)
+        {
+            ObjerctToDrop = ControlerForSklots.ObjectInHand.gameObject;
+        }
         
     }
 
-    // Update is called once per frame
     void Update()
-    {
+    {        
+        if(ControlerForSklots)
+        {
+            ObjerctToDrop = ControlerForSklots.ObjectInHand.gameObject;
+
+            if(ObjerctToDrop && PointForDrop)
+            {
+                if(Input.GetKeyDown(KeyCode.Q))
+                {
+                    Drop(ObjerctToDrop, PointForDrop);
+                    Debug.Log("If is work");
+                }
+                else Debug.Log("If isnt work");
+            }
+        }
+
+        
         
     }
+
+    void Drop(GameObject ObjerctToDrop02, Transform PointForDrop02)
+    {
+        Rigidbody RigidbodyObjectToDrop = ObjerctToDrop02.GetComponent<Rigidbody>();        
+        PutObjects(ObjerctToDrop02, PointForDrop02);
+        
+        
+        Debug.Log("Method 1 is work ");
+
+        void SettingsRigidbody(Rigidbody ReferenceFroRigidbody)
+        {                        
+            ReferenceFroRigidbody.drag = 0.0f;
+            ReferenceFroRigidbody.AngularDrag = 0.5f;
+        
+            ReferenceFroRigidbody.IsKinematic = false;
+            ReferenceFroRigidbody.UseGravity = true;
+            Debug.log("Method 2 is work");
+        }
+    }
+
 }
