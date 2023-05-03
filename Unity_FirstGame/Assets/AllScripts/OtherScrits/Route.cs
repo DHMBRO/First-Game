3dd896267a3c5fd49dc89f944b3f46c86420063b
DHMBRO <127616087+DHMBRO@users.noreply.github.com>
2023-05-03 19:28:31 +0200

    Update

---
+++ Unity_FirstGame/Assets/AllScripts/OtherScrits/Route.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Route : MonoBehaviour
+{    
+    [SerializeField] Transform MyZombie;
+    [SerializeField] List<Transform> MyRoute = new List<Transform>();
+        
+    
+
+    void Start()
+    {
+            
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
--- Unity_FirstGame/Assets/AllScripts/ScrForNps/PatrolScript.cs
+++ Unity_FirstGame/Assets/AllScripts/ScrForNps/PatrolScript.cs
@@ -10,7 +10,9 @@ public class PatrolScript : MonoBehaviour
     [SerializeField] private float Delay = 15.0f;
     [SerializeField] private float MovingTime;    
     [SerializeField] private float SpeedForMove = 10.0f;
+    
     private byte Counter = 0;
+    
     
     void Start()
     {
@@ -27,51 +29,36 @@ public class PatrolScript : MonoBehaviour
         bool InPoint01 = transform.position == Point[0].position;
         bool InPoint02 = transform.position == Point[1].position;
 
-        bool CanMoveToPoint01 = false;
-        bool CanMoveToPoint02 = false;
+        bool MoveToPoint01 = false;
+        bool MoveToPoint02 = false;
 
-        Debug.Log("1");
-        if (Input.GetKeyDown(KeyCode.G))
+        if (Input.GetKeyUp(KeyCode.G) && Counter == 1)
         {
-            Debug.Log("2");
+            Counter = 0;
+        }
+        //
+        if (Input.GetKeyDown(KeyCode.G) && Point.Count > 1)
+        {
             if (InPoint01 && Counter == 0)
             {
                 transform.rotation = Quaternion.LookRotation(RotateToPint02);
-                CanMoveToPoint02 = true;
+                MoveToPoint02 = true;
                 Counter = 1;
-                Debug.Log("3");
             }
             else if (InPoint02 && Counter == 0)
             {
                 transform.rotation = Quaternion.LookRotation(RotateToPint01);
-                CanMoveToPoint01 = true;
+                MoveToPoint01 = true;
                 Counter = 1;
-                Debug.Log("4");
             }
+            
         }
-        
-        //xif (true) 
+
+        if (MoveToPoint02 && MoveToPoint01)
         {
             transform.localPosition += transform.forward * SpeedForMove;
-            
-            Debug.Log("IsMoving !");
         }
         
-        if (InPoint02) 
-        {
-            CanMoveToPoint02 = false;
-            Debug.Log("MoveToPoint02 = false");
-        }
-        if (InPoint01) 
-        {
-            CanMoveToPoint01 = false;
-            Debug.Log("MoveToPoint01 = false");
-        }
-        if (Input.GetKeyUp(KeyCode.G) && Counter == 1)
-        {
-            Counter = 0;
-        }    
-
     }
 
     
