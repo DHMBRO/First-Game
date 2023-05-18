ba9083bf3faf16872cdcf8742040b21521ee9dda
DM <127616087+DHMBRO@users.noreply.github.com>
2023-05-18 06:45:23 +0200

    Update

--- Unity_FirstGame/Assets/AllScripts/ScForPlayer/CamFirstFace.cs
+++ Unity_FirstGame/Assets/AllScripts/ScForPlayer/CamFirstFace.cs
@@ -18,7 +18,7 @@ public class CamFirstFace : MonoBehaviour
 
     void Update()
     {
-        if (/*!uiControler.inventoryIsOpen*/true)
+        if (true)
         {
             float MouseX = Input.GetAxis("Mouse X");
             float MouseY = Input.GetAxis("Mouse Y");
@@ -29,21 +29,7 @@ public class CamFirstFace : MonoBehaviour
             {
                 Player.Rotate(MouseX * new Vector3(0.0f, Sens, 0.0f));
             }
-            /*
-            if (false)
-            {
-                RaycastHit Hitesult;
-                Vector3 TargetPoint = ObjectRay.transform.position + ObjectRay.transform.forward * 100.0f;
-                if (Physics.Raycast(ObjectRay.transform.position, ObjectRay.transform.forward, out Hitesult, 100.0f))
-                {
-                    TargetPoint = Hitesult.point;                    
-                }
-                
-                Quaternion RotateToTarget = Quaternion.LookRotation(TargetPoint - PlayerHand.position);
-                PlayerHand.rotation = Quaternion.Lerp(PlayerHand.rotation, RotateToTarget, 1.0f);
-
-            }
-            */
+           
 
         }
     }
