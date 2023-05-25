7c8df8e509a326de70caa491b73fd415a9720bc2
DM <127616087+DHMBRO@users.noreply.github.com>
2023-05-25 16:50:20 +0200

    Update

--- Unity_FirstGame/Assets/AllScripts/ScForPlayer/DropControler.cs
+++ Unity_FirstGame/Assets/AllScripts/ScForPlayer/DropControler.cs
@@ -22,10 +22,20 @@ public class DropControler : MethodsFromDevelopers
             if (!ControlerForSlots.ObjectInHand.CompareTag("Knife"))
             {
                 DropObjects(ControlerForSlots.ObjectInHand.transform, ObjectToDrop.transform);
+
+                if (ControlerForSlots.MyWeapon01 && ControlerForSlots.ObjectInHand.gameObject == ControlerForSlots.MyWeapon01.gameObject)
+                    ControlerForSlots.MyWeapon01 = null;
+
+                if (ControlerForSlots.MyWeapon02 && ControlerForSlots.ObjectInHand.gameObject == ControlerForSlots.MyWeapon02.gameObject)
+                    ControlerForSlots.MyWeapon02 = null;
+
+                if (ControlerForSlots.MyPistol01 &&  ControlerForSlots.ObjectInHand.gameObject == ControlerForSlots.MyPistol01.gameObject)
+                    ControlerForSlots.MyPistol01 = null;
+
             }
         }
     }
 
-    
+   
 
 }
