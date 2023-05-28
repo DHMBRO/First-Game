f1c44a87172f3f9d4df48fc8946c95094786d67a
DM <127616087+DHMBRO@users.noreply.github.com>
2023-05-28 17:10:01 +0200

    Update

--- Unity_FirstGame/Assets/AllScripts/ScrForImageLoot/SlotsImage.cs
+++ Unity_FirstGame/Assets/AllScripts/ScrForImageLoot/SlotsImage.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SlotsImage : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //All Weapons
+    [SerializeField] GameObject Weapon01;
+    [SerializeField] GameObject Weapon02;
+    [SerializeField] GameObject Knife01;
+    // Shops
+    [SerializeField] GameObject Shop01;
+    [SerializeField] GameObject Shop02;
+    [SerializeField] GameObject Shop03;
+    //Equipment
+    [SerializeField] GameObject Helmet;
+    [SerializeField] GameObject Armor;
+    [SerializeField] GameObject BackPack;
 
-    // Update is called once per frame
     void Update()
     {
-        
+                
     }
 }
