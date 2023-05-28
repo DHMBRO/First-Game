250ba6d4a624646859b681f244b73de3db1d09a5
DM <127616087+DHMBRO@users.noreply.github.com>
2023-05-28 17:15:33 +0200

    Update

--- Unity_FirstGame/Assets/AllScripts/ScrForImageLoot/SlotsImage.cs
+++ Unity_FirstGame/Assets/AllScripts/ScrForImageLoot/SlotsImage.cs
@@ -4,20 +4,27 @@ using UnityEngine.UI;
 public class SlotsImage : MonoBehaviour
 {
     //All Weapons
-    [SerializeField] GameObject Weapon01;
-    [SerializeField] GameObject Weapon02;
-    [SerializeField] GameObject Knife01;
+    [SerializeField] Sprite Weapon01;
+    [SerializeField] Sprite Weapon02;
+    [SerializeField] Sprite Knife01;
     // Shops
-    [SerializeField] GameObject Shop01;
-    [SerializeField] GameObject Shop02;
-    [SerializeField] GameObject Shop03;
-    //Equipment
-    [SerializeField] GameObject Helmet;
-    [SerializeField] GameObject Armor;
-    [SerializeField] GameObject BackPack;
+    [SerializeField] Sprite Shop01;
+    [SerializeField] Sprite Shop02;
+    [SerializeField] Sprite Shop03;
+    //Equipment     
+    [SerializeField] Sprite Helmet;
+    [SerializeField] Sprite Armor;
+    [SerializeField] Sprite BackPack;
+
+    [SerializeField] SlotControler ControlerBySlots;
 
     void Update()
     {
-                
+                    
+
+
     }
+    
+
+
 }
