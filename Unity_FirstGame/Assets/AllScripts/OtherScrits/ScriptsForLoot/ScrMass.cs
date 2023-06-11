bd93263ea246dc98ba7f0d2b8835d01abbdb1127
DM <127616087+DHMBRO@users.noreply.github.com>
2023-06-11 13:15:23 +0200

    Update

--- Unity_FirstGame/Assets/AllScripts/OtherScrits/ScriptsForLoot/ScrMass.cs
+++ Unity_FirstGame/Assets/AllScripts/OtherScrits/ScriptsForLoot/ScrMass.cs
@@ -6,21 +6,6 @@ public class ScrMass: MonoBehaviour
     
     void Start()
     {
-        if (gameObject.CompareTag("Ammo9MM"))
-        {
-            Mass = 0.35f;
-        }
-        else if (gameObject.CompareTag("Ammo45_APC"))
-        {
-            Mass = 0.4f;
-        }
-        else if (gameObject.CompareTag("Ammo5_56MM"))
-        {
-            Mass = 1.0f;
-        }    
-        else if (gameObject.CompareTag("Ammo7_62MM"))
-        {
-            Mass = 1.5f;
-        }        
+       
     }
 }
