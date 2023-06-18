263d01b7e4ef0baeeaa67db8d2e7c7d72497d092
awaqerer <127976085+awaqerer@users.noreply.github.com>
2023-06-18 20:57:47 +0300

    sdfg

--- Unity_FirstGame/Assets/AllScripts/ScrForNps/ZombieController.cs
+++ Unity_FirstGame/Assets/AllScripts/ScrForNps/ZombieController.cs
@@ -18,10 +18,7 @@ public class ZombieController : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawSphere(ZombiePatrolScript.ZombieNavMesh.destination + Vector3.up * 2, 0.5f);
-        Gizmos.color = Color.red;
-        Gizmos.DrawSphere(ZombieLocateScript.Target.transform.position + Vector3.up * 2, 0.5f);
+        
     }
     void Update()
     {
@@ -34,11 +31,11 @@ public class ZombieController : MonoBehaviour
                 //перевірка чи я не відійшов від дестенейшена!
                 if (ZombieLocateScript.CanISeeTarget() && IsLive) 
                 {
-                    Debug.Log("I SEE Target");
+                    
                     if (IsLive && ZombiePatrolScript.ZombieNavMesh.remainingDistance <= ZombieAttackScript.AttackDistance &&
                             (ZombieLocateScript.Target.transform.position - ZombiePatrolScript.ZombieNavMesh.destination).magnitude <= ZombieAttackScript.GoingDistance)
                     {
-                        Debug.Log("WORK");
+                        
 
                         ZombieAttackScript.DoCloseAttack(ZombieLocateScript.Target);
                     }
