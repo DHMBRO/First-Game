e232431ce619fdfc55b32ebd312908b9535e81d1
DM <127616087+DHMBRO@users.noreply.github.com>
2023-06-21 20:04:46 +0200

    Revert "Revert "Merge branch 'main' of https://github.com/DHMBRO/First-Game""
    This reverts commit 429b8185bd2e807b0d57e8033633a3b4f108f8e8.

--- Unity_FirstGame/Assets/AllScripts/ScrForNps/AttackMethod.cs
+++ Unity_FirstGame/Assets/AllScripts/ScrForNps/AttackMethod.cs
@@ -15,6 +15,7 @@ public class AttackMethod : MonoBehaviour
     protected HpScript TargetHpScript;
     protected LocateScript ZombieLocateScript;
     protected PatrolScriptNavMesh ZombiePatrolScript;
+    
 
     void Start()
     {
@@ -30,16 +31,28 @@ public class AttackMethod : MonoBehaviour
 
     public void DoCloseAttack(GameObject Target)
     {
+
         if (Time.time >= AttackTime)
         {
-            if (Target.GetComponentInParent<HpScript>())
+            HpScript TargetHpScript = Target.GetComponentInParent<HpScript>(); 
+            if (TargetHpScript)
             {
+<<<<<<< Updated upstream
+                TargetHpScript.InflictingDamage(ZombieDamage);
+            }
+            
+            AttackTime = AttackDelay + Time.time; 
+        }  
+        
+=======
                 Target.GetComponentInParent<HpScript>()?.InflictingDamage(ZombieDamage);
-                Debug.Log("Work in original");
                 AttackTime = AttackDelay + Time.time;
             }
+
+
         }
 
+>>>>>>> Stashed changes
     }
 
 }
