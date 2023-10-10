e003fa0af587918016f61ef282bd4a5a31ca7f47
DM <127616087+DHMBRO@users.noreply.github.com>
2023-10-10 21:50:20 +0200

    Add use ammo

--- Unity_FirstGame/Assets/AllScripts/ScrForLoot/ScrForUse/ScrForUseAmmo.cs
+++ Unity_FirstGame/Assets/AllScripts/ScrForLoot/ScrForUse/ScrForUseAmmo.cs
@@ -12,10 +12,53 @@ public class ScrForUseAmmo : MonoBehaviour, IUsebleInterFace
     public void Use(GameObject Target, InfoForLoot InfoLoot, SelectAnObject SelectObj)
     {
         SlotControler ContrlerToSlots = Target.GetComponent<SlotControler>();
+        
         List<ShopControler> AllShopsHave = new List <ShopControler>();
+        List<ShopControler> NotFullMag = new List<ShopControler>();
+        List<ShopControler> ShopCanReload = new List<ShopControler>();
+        ShopControler ShopToReload = new ShopControler();
 
         for (int i = 0; i < ContrlerToSlots.Shop.Length;i++) if (ContrlerToSlots.Shop[i]) AllShopsHave.Add(ContrlerToSlots.Shop[i].GetComponent<ShopControler>());
+        for (int i = 0; i < AllShopsHave.Count; i++) if (AllShopsHave[i].CurrentAmmo != AllShopsHave[i].MaxAmmo) NotFullMag.Add(AllShopsHave[i]);
+        for (int i = 0; i < NotFullMag.Count; i++) if (NotFullMag[i].CaliberToShop == CaliberToBox) ShopCanReload.Add(NotFullMag[i]);
+
+        int NotEnoughAmmo;
+
         
+
+
+        if (ShopCanReload.Count == 1) ShopToReload = ShopCanReload[0];
+        else if (ShopCanReload.Count == 2)
+        {
+            if (ShopCanReload[0].CurrentAmmo >= ShopCanReload[1].CurrentAmmo) ShopToReload = ShopCanReload[0];
+            else if(ShopCanReload[0].CurrentAmmo < ShopCanReload[1].CurrentAmmo) ShopToReload = ShopCanReload[1];
+        }
+        else if (ShopCanReload.Count == 3)
+        {
+            if (ShopCanReload[0].CurrentAmmo >= ShopCanReload[1].CurrentAmmo) ShopToReload = ShopCanReload[0];
+            else if (ShopCanReload[0].CurrentAmmo < ShopCanReload[1].CurrentAmmo) ShopToReload = ShopCanReload[1];
+            if (ShopToReload.CurrentAmmo < ShopCanReload[2].CurrentAmmo) ShopToReload = ShopCanReload[2];
+        }
+
+        if (ShopToReload ) ReloadMag();
+
+        void ReloadMag()
+        {
+            NotEnoughAmmo = ShopToReload.MaxAmmo - ShopToReload.CurrentAmmo;
+
+            if (NotEnoughAmmo <= CurrentAmmo)
+            {
+                ShopToReload.CurrentAmmo = ShopToReload.MaxAmmo;
+                CurrentAmmo -= NotEnoughAmmo;
+            }
+            else
+            {
+                ShopToReload.CurrentAmmo += CurrentAmmo;
+                CurrentAmmo = 0;
+            }
+        }
+
+
         if (CurrentAmmo == 0)
         {
             Debug.Log("Name Object:" + gameObject.name);
@@ -29,9 +72,17 @@ public class ScrForUseAmmo : MonoBehaviour, IUsebleInterFace
             InfoLoot.SaveInfo(gameObject);
             Destroy(gameObject);
         }
+    
     }
+ 
+    
+
 
 }
+
+/*        
+        
+
 
 /*
  
