091cb601cd4171e3a9401cfd5e1692ac6b198a93
DM <127616087+DHMBRO@users.noreply.github.com>
2023-12-12 18:38:37 +0100

    Update

--- Unity_FirstGame/Assets/AllScripts/ScrForControlAnimations/ControlerAnimationsBandit.cs
+++ Unity_FirstGame/Assets/AllScripts/ScrForControlAnimations/ControlerAnimationsBandit.cs
@@ -1,16 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ControlerAnimationsBandit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] Animation Parameters;    
+    
     void Update()
     {
         
--- Unity_FirstGame/Assets/AllScripts/ScrForControlAnimations/ControlerAnimationsZombie.cs
+++ Unity_FirstGame/Assets/AllScripts/ScrForControlAnimations/ControlerAnimationsZombie.cs
@@ -1,18 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ControlerAnimationsZombie : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] Animation Parameters;
+    
     void Update()
     {
-        
+            
     }
 }
