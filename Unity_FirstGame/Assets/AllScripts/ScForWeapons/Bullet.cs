62491bf89b2e70e34459579d4f509072fb971e3b
DM <127616087+DHMBRO@users.noreply.github.com>
2024-01-17 21:13:29 +0100

    Fixed bullets, prefab helicopter, aded new folders for helicopter

--- Unity_FirstGame/Assets/AllScripts/ScForWeapons/Bullet.cs
+++ Unity_FirstGame/Assets/AllScripts/ScForWeapons/Bullet.cs
@@ -9,19 +9,42 @@ public class Bullet : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         //audio = GameObject.Instantiate(audioToMetalic);
-        Debug.Log("Bullet Collide with " + collision.gameObject.name);
         HpScript HPScr = collision.gameObject.GetComponent<HpScript>();
-        if (HPScr && Damage > 0.0f)
+
+
+        //Debug.Log("Bullet Collide with " + collision.gameObject.name);
+
+        if (HPScr)
+        {
+            GiveDamage(HPScr);
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        HpScript HPScr = other.gameObject.GetComponent<HpScript>();
+
+
+        //Debug.Log("Bullet Collide with " + other.gameObject.name);
+
+        if (HPScr)
+        {
+            GiveDamage(HPScr);
+        }
+    }
+
+    void GiveDamage(HpScript HPScr)
+    {
+        if (Damage > 0.0f)
         {
             
             HPScr.InflictingDamage(Damage);
             Debug.Log(HPScr.gameObject.name + " Bullet Hit " + HPScr.HealthPoint);
-
-
         }
 
         Destroy(gameObject);
-        
+
     }
 
     void Start()
