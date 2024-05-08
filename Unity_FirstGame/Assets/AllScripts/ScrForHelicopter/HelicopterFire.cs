a9b1c149664a28e7726c4740f754c0357a560c62
ArtemLabutkin <127979955+ArtemLabutkin@users.noreply.github.com>
2024-05-08 20:57:03 +0300

    update vfx and Helicopter shooting

--- Unity_FirstGame/Assets/AllScripts/ScrForHelicopter/HelicopterFire.cs
+++ Unity_FirstGame/Assets/AllScripts/ScrForHelicopter/HelicopterFire.cs
@@ -6,27 +6,41 @@ using UnityEngine;
 public class HelicopterFire : MonoBehaviour
 {
     [SerializeField]GameObject Ammo;
-    float timer = 0.00f;
-    [SerializeField] HelicopterScr Hscr; 
+    float timerShootRate = 0.00f;
+    float timerShooting = 0.00f;
+
+    [SerializeField] float ShootDelay = 0.16f;
+    [SerializeField] HelicopterScr Hscr;
+    [SerializeField] float SootingRandomSceMin;
+    [SerializeField] float SootingRandomSecMax;
+    float timeOfSet;
+    bool fire = true;
     // Start is called before the first frame update
     void Start()
     {
-
+        timeOfSet = Random.Range(SootingRandomSceMin, SootingRandomSecMax);
     }
     // Update is called once per frame
     void Update()
     {
         if (Hscr.State == HelicopterScr.States.RoamAround)
         {
-            timer += Time.deltaTime;
+            timerShootRate += Time.deltaTime;
+            timerShooting += Time.deltaTime;
             //Debug.Log("HelicopterFire" + " " + timer);
             if (Hscr.TargetToGun)
             {
-                if (0.01f <= timer)
+                if(timerShooting > timeOfSet)
+                {
+                    fire = !fire;
+                    timeOfSet = Random.Range(SootingRandomSceMin, SootingRandomSecMax);
+                    timerShooting = 0.00f;
+                }
+                if (ShootDelay <= timerShootRate && fire)
                 {
                     GameObject Amo = Instantiate(Ammo, gameObject.transform.position, gameObject.transform.rotation);
                     Destroy(Amo, 10f);
-                    timer = 0.00f;
+                    timerShootRate = 0.00f;
                 }
             }
         }
