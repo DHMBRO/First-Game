b35b4939fa6ce4af2197abd9423625555a88c633
DM <127616087+DHMBRO@users.noreply.github.com>
2024-06-14 18:38:51 +0200

    Update with IK

--- Unity_FirstGame/Assets/AllScripts/ScrForControlAnimations/IK/IKControlerForPlayer.cs
+++ Unity_FirstGame/Assets/AllScripts/ScrForControlAnimations/IK/IKControlerForPlayer.cs
@@ -2,14 +2,20 @@ using UnityEngine;
 
 public class IKControlerForPlayer : MonoBehaviour
 {
+    [SerializeField] Transform PlayerPivot = null;
+    AimControler ContrlerAim;
+
     Animator PlayerControlerAnimator;
     PlayerControler ControlerPlayer;
-    
-    [SerializeField] Transform LeftArmPosition;
-    
+    ShootControler ControlerShoot; 
+
+    private Transform LeftArmPosition;
+    private Quaternion OffSetRotationRightArm = new Quaternion();
+
     void Start()
     {
         ControlerPlayer = GetComponentInParent<PlayerControler>();
+        ContrlerAim = GetComponentInParent<AimControler>();
         PlayerControlerAnimator = GetComponent<Animator>(); ;
     }
 
@@ -22,10 +28,21 @@ public class IKControlerForPlayer : MonoBehaviour
 
         if (ControlerPlayer.ControlerShoot)
         {
+            ControlerShoot = ControlerPlayer.ControlerShoot;
             LeftArmPosition = ControlerPlayer.ControlerShoot.LeftArmPositionIK;
         }
         else LeftArmPosition = null;
 
+    }
+
+    private void Update()
+    {
+        if (PlayerPivot)
+        {
+            OffSetRotationRightArm = Quaternion.LookRotation((ContrlerAim.GetLookPoint() - PlayerPivot.localPosition));
+            OffSetRotationRightArm.eulerAngles = new Vector3(OffSetRotationRightArm.eulerAngles.x, OffSetRotationRightArm.eulerAngles.y, -90.0f);
+        }
+        
     }
 
     private void OnAnimatorIK(int layerIndex)
@@ -39,10 +56,14 @@ public class IKControlerForPlayer : MonoBehaviour
         }
 
         PlayerControlerAnimator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1.0f);
+
         PlayerControlerAnimator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1.0f);
+        PlayerControlerAnimator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1.0f);
 
         PlayerControlerAnimator.SetIKPosition(AvatarIKGoal.LeftHand, LeftArmPosition.position);
         PlayerControlerAnimator.SetIKRotation(AvatarIKGoal.LeftHand, LeftArmPosition.rotation);
+
+        PlayerControlerAnimator.SetIKRotation(AvatarIKGoal.RightHand, OffSetRotationRightArm);
 
     }
 
